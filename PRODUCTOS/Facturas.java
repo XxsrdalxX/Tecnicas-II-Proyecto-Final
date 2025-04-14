c6199b8f1a953e74f8b773f5d4ed114b68735601
Dylan Càrdenas <cardenasdylan58@gmail.com>
2025-04-14 11:40:19 -0500

    Commit bueno
    xd

--- PRODUCTOS/Facturas.java
+++ PRODUCTOS/Facturas.java
@@ -1,3 +1,3 @@
-public interface Factursa {
+public interface Facturas {
 
 }
--- PRODUCTOS/Productos.java
+++ PRODUCTOS/Productos.java
@@ -1,3 +1,54 @@
-public class Productos {
+public abstract class Productos {
+protected String nombre;
+protected String descripcion;
+protected double precio;
+protected int cantidadEnStock;
+protected String categoria;
+protected int fechaDeCaducidad;
 
+public Productos(String nombre, String descripcion, double precio, int cantidadEnStock) { 
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.cantidadEnStock = cantidadEnStock;
+}
+
+
+
+
+
+
+// Getters y Setters
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+
+public String getDescripcion() {
+    return descripcion;
+}
+
+public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+}
+
+public double getPrecio() {
+    return precio;
+}
+
+public void setPrecio(double precio) {
+    this.precio = precio;
+}
+
+public int getCantidadEnStock() {
+    return cantidadEnStock;
+}
+
+public void setCantidadEnStock(int cantidadEnStock) {
+    this.cantidadEnStock = cantidadEnStock;
+}
 }
